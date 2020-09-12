35a801b2a71ec76297b655b4f43829c71d9d84c8
Ravi Teja Gudapati <tejainece@gmail.com>
2020-09-13 00:01:08 +0200

    spritesheet filltype

--- web/spritesheet/main.dart
+++ web/spritesheet/main.dart
@@ -17,12 +17,44 @@ Future<void> main() async {
   final objects = <Object2D>[];
 
   {
-    final rect = Object2D.rectangularMesh(
-        renderer, Rectangle(10, 10, 128, 128),
+    final rect = Object2D.rectangularMesh(renderer, Rectangle(10, 10, 128, 128),
         background: Background(
             color: Color(r: 0.5, g: 0.5, a: 1),
             image: ImageProperties(renderer.getTexture('spritesheet'),
                 textureRegion: Rectangle(128, 128, 128, 128))));
+    objects.add(rect);
+  }
+
+  {
+    final rect = Object2D.rectangularMesh(
+        renderer, Rectangle(150, 10, 256, 256),
+        background: Background(
+            color: Color(r: 0.5, g: 0.5, a: 1),
+            image: ImageProperties(renderer.getTexture('spritesheet'),
+                textureRegion: Rectangle(128, 128, 128, 128),
+                fillType: FillType.repeat)));
+    objects.add(rect);
+  }
+
+  {
+    final rect = Object2D.rectangularMesh(
+        renderer, Rectangle(450, 10, 256, 256),
+        background: Background(
+            color: Color(r: 0.5, g: 0.5, a: 1),
+            image: ImageProperties(renderer.getTexture('spritesheet'),
+                textureRegion: Rectangle(128, 128, 128, 128),
+                fillType: FillType.cover)));
+    objects.add(rect);
+  }
+
+  {
+    final rect = Object2D.rectangularMesh(
+        renderer, Rectangle(750, 10, 100, 100),
+        background: Background(
+            color: Color(r: 0.5, g: 0.5, a: 1),
+            image: ImageProperties(renderer.getTexture('spritesheet'),
+                textureRegion: Rectangle(128, 128, 128, 128),
+                fillType: FillType.contain)));
     objects.add(rect);
   }
 
